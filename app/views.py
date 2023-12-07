10c03cabb3eafdfade42035b02bce6c48fb267bb
Antoine Verlyck <a.verlyck@2s-agency.fr>
2023-12-07 12:29:40 +0100

    add index and detail view in app/views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Car
+
+
+def detail(request, car_id):
+    return HttpResponse("You're looking at car %s." % car_id)
+
+
+def index(request):
+    latest_car_list = Car.objects.order_by('-id')[:5]
+    context = {'latest_car_list': latest_car_list}
+    return render(request, 'app/index.html', context)
