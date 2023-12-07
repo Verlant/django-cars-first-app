127a83b415c8e6e3cedc99cd8124fb8d43df9858
Antoine Verlyck <a.verlyck@2s-agency.fr>
2023-12-07 11:54:11 +0100

    Car model created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+# La marque
+# Le modèle
+# L’année de construction
+# La cylindrée
+# La version (break, coupé etc.)
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#     def __str__(self):
+#         return self.question_text
+#
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 # Create your models here.
+class Car(models.Model):
+    marque = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    annee_construction = models.DateField('annee_construction')
+    cylindree = models.CharField(max_length=200)
+    version = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.marque + " " + self.model
